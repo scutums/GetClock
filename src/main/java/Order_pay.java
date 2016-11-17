b5675985b92e6493a61366758889696dd64df9c1
scutums <stepin.ua@gmail.com>
2016-11-18 00:41:17 +0200

    Добить гарантию, сделать новый запрос + распечатку.

--- src/main/java/Order_pay.java
+++ src/main/java/Order_pay.java
@@ -1,9 +1,11 @@
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Date;
+import java.util.Calendar;
 
 /**
  * Created by Alexander on 17.11.2016.
@@ -16,6 +18,12 @@ public class Order_pay extends SQL
     private Label end_line;
     @FXML
     private Label end;
+    @FXML
+    private Label vals;
+    @FXML
+    private Label pays;
+    @FXML
+    private Label sym;
 
     private String local_time;
     private String stik;
@@ -29,19 +37,21 @@ public class Order_pay extends SQL
     @FXML
     private TableColumn<Pay_End, String> master_end;
     @FXML
-    private TableColumn<Pay_End, String> date_start;
+    private TableColumn<Pay_End, String> start_end;
     @FXML
     private TableColumn<Pay_End, String> dat_end;
     @FXML
-    private TableColumn<Pay_End, String> client_end;
+    private TableColumn<Pay_End, String> client;
     @FXML
-    private TableColumn<Pay_End, Integer> phone_end;
+    private TableColumn<Pay_End, Integer> phone;
     @FXML
     private TableColumn<Pay_End, String> note_end;
     @FXML
     private TableColumn<Pay_End, Integer> value;
     @FXML
     private TableColumn<Pay_End, Integer> payment;
+    @FXML
+    private ComboBox comboBox;
 
 
     @FXML
@@ -49,12 +59,17 @@ public class Order_pay extends SQL
     @FXML
     private TextField poisk_tel;
 
+    private String date_garant;
+
     @FXML
     private void initialize() {
 
         LOCAL_TIME();
         Clitent_table();
       //  end.setVisible(false);
+        TABLE_CLICK();
+        filter();
+        Box();
 
     }
     private void LOCAL_TIME() //Время на машине.
@@ -69,23 +84,115 @@ public class Order_pay extends SQL
     public void Clitent_table() //вывод таблицы клиентов
     {
         BD_PAY_TABLE();
-        System.out.println(Pay_Data);
         stick_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("stick_end"));
         model_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("model_end"));
         master_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("master_end"));
-
-      //  date_start.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("date_start"));
-
+        start_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("start_end"));
         dat_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("dat_end"));
-
-       // client_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("client_end"));
-       // phone_end.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("phone_end"));
-
+        client.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("client"));
+        phone.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("phone"));
         note_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("note_end"));
-        value.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("value"));
-        payment.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("payment"));
-
+       // value.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("value"));
+       // payment.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("payment"));
         table_pay.setItems(Pay_Data);
+    }
+
+    public void TABLE_CLICK() {
+        table_pay.getSelectionModel().selectedItemProperty().addListener(
+                (obserdler, oldValue, newValue) -> Selectional(newValue)
+        );
+    }
+
+    public void Selectional(Pay_End line) {
+        stik  = line.getStick_end();
+        String mod = line.getModel_end();
+        String fio = line.getClient();
+        Integer ph = line.getPhone();
+        Integer val = line.getValue();
+        Integer pay = line.getPayment();
+        int x = val-pay;
+        end_line.setText("Заказ № " + stik + ".   Модель : " + mod + ".   Имя владельца : " + fio + ".   Телефон : " + ph + ".  -ВЫБРАН.");
+        sym.setText("Общая сумма : " + val);
+        vals.setText("Было уплачено : " + pay);
+        pays.setText("Требуется доплатить : " + x);
+    }
+
+    public void filter() {
+        try {
+            stick_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("stick_end"));
+            master_end.setCellValueFactory(new PropertyValueFactory<Pay_End, String>("master_end"));
+            phone.setCellValueFactory(new PropertyValueFactory<Pay_End, Integer>("phone"));
+
+
+            FilteredList<Pay_End> filteredData = new FilteredList<>(Pay_Data, p -> true);
+
+            poisk.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(person -> {
+
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (person.getStick_end().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    }
+                    else /*if(person.getMaster_end().toLowerCase().contains(lowerCaseFilter))*/ if(Integer.toString(person.getPhone()).indexOf(newValue) != -1)
+                    {
+                    return true;
+                    }
+                    return false;
+                });
+            });
+
+
+            SortedList<Pay_End> sortedData = new SortedList<>(filteredData);
+
+
+            sortedData.comparatorProperty().bind(table_pay.comparatorProperty());
+
+
+            table_pay.setItems(sortedData);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void Box()
+    {
+        comboBox.getItems().addAll(
+                "Нет гарантии",
+                "Гарантия на пол года (182 дня)",
+                "Гарантия на год (365 дней)"
+        );
+      String com = comboBox.getId();
+        System.out.println(com);
+    }
+
+    @FXML
+    void lopi()
+    {
+        String com = (String) comboBox.getValue();
+
+        if (com == "Нет гарантии")
+        {
+            end.setText("Тарантия отсуцтвуе");
+            date_garant = local_time;
+        }
+        if (com == "Гарантия на пол года (182 дня)")
+        {
+            date_garant = local_time + 182;
+            end.setText("Конечная дата : " + date_garant);
+
+        }
+        if (com == "Гарантия на год (365 дней)")
+        {
+            date_garant = local_time + 365;
+            end.setText("Конечная дата : " + date_garant);
+        }
     }
 
 
